a3f68f4d10c69819266d29606718c54dcb1e03a7
samar555 <sachinmishraravi@gmail.com>
2025-04-22 12:00:17 +0530

    added navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+
+    path:"/",
+    loadComponent:()=>import("./core/components/header/header.component").then(c=>c.HeaderComponent)
+}];
