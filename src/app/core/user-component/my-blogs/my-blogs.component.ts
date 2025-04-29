a4538d414bc71dee0d054f5c9aee4d26bc3b842d
samar555 <sachinmishraravi@gmail.com>
2025-04-29 19:09:21 +0530

    added blog

---
+++ src/app/core/user-component/my-blogs/my-blogs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-blogs',
+  imports: [],
+  templateUrl: './my-blogs.component.html',
+  styleUrl: './my-blogs.component.css'
+})
+export class MyBlogsComponent {
+
+}
